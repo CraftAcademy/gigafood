21410172ab20570cc24d438a8c480535322dadb7
satorinic <niccarlsson@gmail.com>
2017-09-11 10:02:30 +0200

    add configuration for english as default lang

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Gigafood
       generate.controller_specs false
     end
 
+    config.i18n.available_locales = :sv
+    config.i18n.default_locales = :en
+
     config.generators.system_tests = nil
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
