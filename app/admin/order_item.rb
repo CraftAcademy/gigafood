a1d52a253a1bfa07e5bdd3b50673f88f09b1571d
j0sv <svensson.jonas@gmail.com>
2017-09-14 20:51:22 +0200

    Remove comment

--- app/admin/order_item.rb
+++ app/admin/order_item.rb
@@ -1,17 +1,7 @@
 ActiveAdmin.register OrderItem do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :owner, :item, :list, :of, :attributes, :on, :model,
+
+  permit_params :owner, :item, :list, :of, :attributes, :on, :model,
   :owner_id, :owner_type, :quantity, :item_id, :item_type, :price_cent,
   :price_currency, :created_at, :updated_at
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
 end
