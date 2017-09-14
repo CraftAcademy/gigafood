a4cb0ef48c2208e6f3794ee770c63fcea0bab9eb
j0sv <svensson.jonas@gmail.com>
2017-09-14 15:30:54 +0200

    Modify links

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -35,7 +35,9 @@ ActiveAdmin.register Order do
       column :item
       column :price
       column :quantity
-      #row('delete') { link_to 'Delete', admin_order_item(order.shopping_cart_items.id) }
+      column :Edit { link_to 'Edit', admin_order_item_path(40), method: :put }
+      column :Show { link_to 'Show', admin_order_item_path(40) }
+      column :Delete { link_to 'Delete', admin_order_item_path(19), method: :delete }
     end
 
     attributes_table do
