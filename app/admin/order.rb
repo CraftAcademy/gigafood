4bd28494b5b2f425b15df9fde2057b165e0417fc
faraznaeem <faraznaeem@gmail.com>
2017-09-14 12:14:32 +0200

    Add table for order items

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Order do
   permit_params :delivery_date, :delivery_method, :delivery_name, :delivery_address, :delivery_postal_code, :delivery_city,
                 :delivery_floor, :delivery_door_code, :delivery_contact_name, :delivery_contact_phone_number, :billing_name,
                 :billing_company, :billing_org_nr, :billing_address, :billing_postal_code, :billing_city, :billing_phone,
-                :billing_email, :allergies, :boxes, :status
+                :billing_email, :allergies, :boxes, :status, :shopping_cart_items, :order_item
 
   action_item :confirm_order, only: :show do
     link_to 'Confirm Order', confirm_admin_order_path(resource), method: :put
@@ -12,6 +12,10 @@ ActiveAdmin.register Order do
   action_item :cancel_order, only: :show do
     link_to 'Cancel Order', cancel_admin_order_path(resource), method: :put
   end
+
+  # action_item :order_items do
+  #   render order_items, method: :put
+  # end
 
   member_action :confirm, method: :put do
     @order = Order.find(params[:id])
@@ -28,4 +32,38 @@ ActiveAdmin.register Order do
     ConfirmationMailer.cancelation_email(@order).deliver
     redirect_to resource_path, notice: "Canceled!"
   end
+
+  show do
+    h3 'Order Items'
+    table_for order.shopping_cart_items do
+      column :item
+      column :price
+      column :quantity
+    end
+
+    attributes_table do
+      row :delivery_date
+      row :delivery_method
+      row :delivery_name
+      row :delivery_address
+      row :delivery_postal_code
+      row :delivery_city
+      row :delivery_floor
+      row :delivery_door_code
+      row :delivery_contact_name
+      row :delivery_contact_phone_number
+      row :billing_name
+      row :billing_company
+      row :billing_org_nr
+      row :billing_address
+      row :billing_postal_code
+      row :billing_city
+      row :billing_phone
+      row :billing_email
+      row :allergies
+      row :boxes
+      row :status
+    end
+  end
+
 end
