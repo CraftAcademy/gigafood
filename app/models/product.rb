a29055e57c4fec50ccc25f4d22c44574c45f7c55
magnus-thor <magnus.thor.ar@gmail.com>
2018-01-17 12:01:04 +0100

    create migration for Product of_type create scopes for dishes and cutlery

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates_inclusion_of :of_type, in: ['dish', 'cutlery']
+
+  scope :dishes, -> { where(of_type: 'dish')}
+  scope :cutlery, -> { where(of_type: 'cutlery')}
 end
